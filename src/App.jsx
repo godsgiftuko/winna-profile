1f280766eb1fe5b671d638ab07279f60711992fb
God'sgift Uko <godsgiftuko@gmail.com>
2023-03-22 23:49:41 +0100

    Removed extension resolver from vite config. Corrected import error in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Home from './components/home'
+import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/contact'
 import Projects from './components/Projects'
--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    extensions: ['.js', '.jsx',  '.vue', '.json', '.css', '.scss', '.sass', '.less', '.styl']
-  }
+  plugins: [react()]
 })
